52aa2c0960b462c3ed2611f84d2f617bb03f56d4
Mario <you@example.com>
2018-05-27 23:42:17 -0600

    privado en 80%

--- public/code.js
+++ public/code.js
@@ -15,11 +15,7 @@ function submitClick() {
 
     var mensaje = publicMessage.value;
     window.alert(mensaje + " mierda para ponerme a pija");
-    //ref.child("Users").child(user.uid).child("userName").set(user.displayName);
-    /*ref.child("Mensajes").push().child("mensajePublico").set(mensaje);
-    ref.child("Mensajes").push().child("UID").set(user.uid);*/
-    //var textMessage = document.getElementById("publicMessage");
-    //console.log("Este es textMessage: " + textMessage);
+
     console.log("Este es mensaje: " + mensaje);
     ref.child("Mensajes").push().set({
         userName: user.displayName,
@@ -33,38 +29,52 @@ function submitClick() {
         var number = datasnapshot.val();
         console.log("Este es number: " + number);
         castedNumber = Number(number);
-        if (isNaN(castedNumber)) {
+        /*if (isNaN(castedNumber)) {
             castedNumber = 0;
         } else {
             console.log("Despues de la asignación: " + castedNumber);
             castedNumber += 1;
-        }
+        }*/
 
         console.log("Numero de mensajes publicos: " + castedNumber.toString());
-        counterPublic.set(castedNumber.toString());
+        counterPublic.set(number + 1);
     })
-
     printMessages();
-    //ref.child("Usuarios").child(user.uid).child("mensajesPublicos").set();
-
-    /*try {
-        var privateMessages = firebase.database().ref('Users/' + user.uid + '/mensajePublico');
-        console.log("privateMessages: " + privateMessages);
-        if (privateMessages != null) {
-            ref.child("Usuarios").child(user.uid).child("mensajesPublicos").set();
-            privateMessages.on('value', function(snapshot) {
-                updatemensajesPublicos(postElement, snapshot.val());
-            });
-        }
-    } catch (e) {
-        console.log("Error: ", e.stack);
-        console.log("Error: ", e.name);
-        console.log("Error: ", e.message);
-    } finally {
-
-    }*/
 }
-//ref.child("Usuarios").child(user.uid).child("")
+
+function submitClickPrivate() {
+
+    var user = firebase.auth().currentUser;
+    setName(user);
+
+    var mensaje = publicMessage.value;
+    window.alert(mensaje + " mierda para ponerme a pija");
+
+    console.log("Este es mensaje: " + mensaje);
+    ref.child("Mensajes").push().set({
+        userName: user.displayName,
+        UID: user.uid,
+        mensajePrivado: mensaje
+    });
+
+    var counterPublic = firebase.database().ref().child("Usuarios").child(user.uid).child("mensajesPrivados");
+
+    counterPublic.on('value', function(datasnapshot) {
+        var number = datasnapshot.val();
+        console.log("Este es number: " + number);
+        castedNumber = Number(number);
+        /*if (isNaN(castedNumber)) {
+            castedNumber = 0;
+        } else {
+            console.log("Despues de la asignación: " + castedNumber);
+            castedNumber += 1;
+        }*/
+
+        console.log("Numero de mensajes privados: " + castedNumber.toString());
+        //counterPublic.set(number + 1);
+    })
+    printPrivateMessages();
+}
 
 
 //autentificacion INSTANCIA usuario y confirmacion de conexion
@@ -104,5 +114,34 @@ function printMessages() {
             "<p class='card-text'>" + mensaje + "</p>" +
             "<br> <p class='card-text'>" + name + "</p> </div></div>"
         $("#setMessages").append(content);
+    })
+};
+
+function printPrivateMessages() {
+    document.getElementById("setPrivateMessages").innerHTML = "";
+    var user = firebase.auth().currentUser;
+    var refMensajes = firebase.database().ref().child("Mensajes");
+    var refUser = firebase.database().ref().child("Usuarios");
+
+    refMensajes.on("child_added", snap => {
+
+        var name = snap.child("userName").val();
+        console.log("userName: " + name);
+        var mensaje = snap.child("mensajePrivado").val();
+        console.log("mensajePrivado: " + mensaje);
+        var guestUser = snap.child("UID").val();
+        var refUser = firebase.database().ref().child("Usuarios").child(guestUser).child("Photo URL");
+        var guestPhoto;
+        refUser.on('value', function(laFoto) {
+            guestPhoto = laFoto.val();
+        })
+        console.log("Photo URL: " + guestPhoto);
+        var content = "<br><br><div class='card' style='width: 18rem;'>" +
+            "<img class='card-img-top' src='" + guestPhoto + "' alt='Card image cap'>" +
+            "<div class='card-body'>" +
+            "<h5 class='card-title'> Post Title </h5> " +
+            "<p class='card-text'>" + mensaje + "</p>" +
+            "<br> <p class='card-text'>" + name + "</p> </div></div>"
+        $("#setPrivateMessages").append(content);
     })
 };
